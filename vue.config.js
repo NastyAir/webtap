dac00e5608150cf6287eb6a108a033c06478bf13
qwl <1115094858@qq.com>
2018-12-25 09:38:26 +0800

    添加vue disableHostCheck false

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    devServer: {
+        open: true,
+        host: '127.0.0.1',
+        disableHostCheck: true, // 加这个就没事啦。
+        port: 3000,
+        https: false,
+        hotOnly: false,
+        proxy: null,
+        // before: app => {
+        // }
+    },
+}
