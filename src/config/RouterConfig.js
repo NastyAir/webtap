f49463cb643f5e81775093a7cc662bc15a267930
qwl <1115094858@qq.com>
2018-12-27 15:58:30 +0800

    添加预览

--- src/config/RouterConfig.js
+++ src/config/RouterConfig.js
@@ -1,8 +1,10 @@
 import HelloWorld from '../components/HelloWorld'
 import WebSitePage from '../components/WebSitePage'
+import Schedule from '../components/Schedule'
 
 const routes = [
     { path: '/helloworld', component: HelloWorld },
+    { path: '/schedule', component: Schedule },
     { path: '/website', component: WebSitePage }
 ]
 
